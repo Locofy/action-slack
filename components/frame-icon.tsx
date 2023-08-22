abfcbe1f6652bb4f1a614a6372f04a31411cdcbb
aqil-locofy007 <aqil-locofy007>
2023-08-22 10:06:11 +0000

    Pushed from Locofy Builder

---
+++ components/frame-icon.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+import styles from "./frame-icon.module.css";
+const FrameIcon: NextPage = () => {
+  return <img className={styles.frameChild} alt="" src="/frame-5@2x.png" />;
+};
+
+export default FrameIcon;
